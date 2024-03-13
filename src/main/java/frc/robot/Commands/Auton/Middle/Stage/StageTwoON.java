86e9391e61a877bd78dc5670c998564c45996171
James Barnes <62391527+freddytums@users.noreply.github.com>
2024-03-13 14:40:02 -0500

    un-abstracting StageTwoOn for troubleshooting ...
    copy of StageTwoOn is saved ...

--- src/main/java/frc/robot/Commands/Auton/Middle/Stage/StageTwoON.java
+++ src/main/java/frc/robot/Commands/Auton/Middle/Stage/StageTwoON.java
@@ -2,14 +2,28 @@ package frc.robot.Commands.Auton.Middle.Stage;
 
 import frc.robot.RobotContainer;
 import frc.robot.Commands.Auton.AutonCommandBase;
+import frc.robot.Commands.Auton.OneAuton;
 import frc.robot.Commands.Auton.Paths;
 
 public class StageTwoON extends AutonCommandBase{
     public StageTwoON(RobotContainer robotContainer){
         super(robotContainer);
 
+        resetRobotPose(Paths.getInstance().STAGE_SM);
+
         this.addCommands(
-            new StageIN(robotContainer),
+            // new StageIN(robotContainer),
+                
+                    // new OneAuton(robotContainer, Paths.getInstance().STAGE_SM),
+                        // resetRobotPose(Paths.getInstance().STAGE_SM); (moved outside addCommands())
+                        AimAndShoot(robotContainer),
+                    // end
+
+                Follow(Paths.getInstance().STAGE_SM),
+                FollowToIntake(Paths.getInstance().SOURCE_IN),
+                AimAndShoot(robotContainer),
+            // end
+
             FollowToIntake(Paths.getInstance().SOURCE_ON),
             AimAndShoot(robotContainer)
         );
