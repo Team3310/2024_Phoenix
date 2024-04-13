de8141d5064f9fec91426ee7fa07f799143611d3
Zachary Newkirk <zacharynewkirk88@gmail.com>
2024-04-13 14:17:48 -0500

    auton

--- src/main/java/frc/robot/util/test.java
+++ src/main/java/frc/robot/util/test.java
@@ -1,12 +1,14 @@
 package frc.robot.util;
 
 import frc.robot.Constants;
+import frc.robot.util.Choosers.AutonomousChooser.AutonomousMode;
 import frc.robot.util.Interpolable.InterpolatingDouble;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(Constants.kLiftAngleMap.getInterpolated(new InterpolatingDouble(10.0)).value);
-        System.out.println(Constants.kRightShooterMap.getInterpolated(new InterpolatingDouble(10.0)).value);
-        System.out.println(Constants.kLeftShooterMap.getInterpolated(new InterpolatingDouble(10.0)).value);
+        System.out.println(AutonomousMode.values().length);
+        // System.out.println(Constants.kLiftAngleMap.getInterpolated(new InterpolatingDouble(10.0)).value);
+        // System.out.println(Constants.kRightShooterMap.getInterpolated(new InterpolatingDouble(10.0)).value);
+        // System.out.println(Constants.kLeftShooterMap.getInterpolated(new InterpolatingDouble(10.0)).value);
     }
 }
